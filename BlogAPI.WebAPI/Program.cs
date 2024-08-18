16c2c9c8f6fc99df98296f73df35a186daf2c943
maryam safaei <maryam.safaei701@gmail.com>
2024-08-18 20:08:43 +0330

    Add migration Initial

--- BlogAPI.WebAPI/Program.cs
+++ BlogAPI.WebAPI/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<BlogDbContext>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString()));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BlogConnection")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
